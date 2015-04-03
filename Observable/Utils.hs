2fd8054cb310d596dffb30fe2e68fa73874aa856
Jared Tobin <jared@jtobin.ca>
2015-04-03 17:44:43 +1000

    Add extracting utilities.

--- Observable/Utils.hs
+++ Observable/Utils.hs
@@ -6,6 +6,10 @@ import Control.Monad
 import Control.Monad.Primitive
 import Data.Function
 import Data.List
+import Data.Monoid
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Observable.Core
 import System.Random.MWC
 import System.Random.MWC.Distributions
 
@@ -44,4 +48,32 @@ symmetricDirichlet
 symmetricDirichlet n a g = do
   zs <- replicateM n (gamma a 1 g)
   return $ fmap (/ sum zs) zs
+
+extractInt :: String -> Environment Lit -> Int
+extractInt name store = case Map.lookup name store of
+  Nothing -> error $ "parameter '" <> name <> "' not found"
+  Just v  -> case v of
+    LitInt j -> j
+    _ -> error $
+      "expected Integer for parameter '" <> name <> "', got: " <> show v
+
+extractDouble :: String -> Environment Lit -> Double
+extractDouble name store = case Map.lookup name store of
+  Nothing -> error $ "parameter '" <> name <> "' not found"
+  Just v  -> case v of
+    LitDouble j -> j
+    _ -> error $
+      "expected Double for parameter '" <> name <> "', got: " <> show v
+
+extractVec :: String -> Environment Lit -> [Lit]
+extractVec name store = case Map.lookup name store of
+  Nothing -> error $ "parameter '" <> name <> "' not found"
+  Just v  -> case v of
+    LitVec j -> j
+    _ -> error $
+      "expected Vector for parameter '" <> name <> "', got: " <> show v
+
+grabDouble :: String -> Lit -> Double
+grabDouble name (LitDouble j) = j
+grabDouble name _ = error $ "expected Double for value inside '" <> name <> "'"
 
