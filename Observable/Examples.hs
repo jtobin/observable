73c4a151e124cdf91af1f7716f2f7b33214e65ea
Jared Tobin <jared@jtobin.ca>
2015-04-03 21:51:25 +1000

    Add trigFit example.

--- Observable/Examples.hs
+++ Observable/Examples.hs
@@ -29,3 +29,12 @@ linearFit c d xs = do
   let mus = fmap (\v -> a + b * v) xs
   observe "ys" (IsoGauss mus var)
 
+-- | An example Bayesian linear regression model.
+trigFit :: Double -> Double -> [Double] -> Observable [Double]
+trigFit c d xs = do
+  as  <- observe "coeffs" (IsoStandard 3)
+  var <- observe "variance" (Gamma c d)
+  let model v = sum $ zipWith (*) as [1, cos v, sin v]
+  let mus     = fmap model xs
+  observe "ys" (IsoGauss mus var)
+
