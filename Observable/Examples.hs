edc536f9d1b2ef561675d18ab15a0d1fbc4e3d57
Jared Tobin <jared@jtobin.ca>
2015-08-10 15:48:00 -0400

    Misc cleanup.

--- Observable/Examples.hs
+++ Observable/Examples.hs
@@ -28,36 +28,36 @@ posteriorBb = condition (parameters [("x", discrete 3)]) exampleBb
 -- | An example Bayesian linear regression model.
 linearFit :: Double -> Double -> [Double] -> Observable [Double]
 linearFit c d xs = do
-  a   <- observe "intercept" standard
-  b   <- observe "slope" standard
-  var <- observe "variance" (gamma c d)
+  a <- observe "intercept" standard
+  b <- observe "slope" standard
+  v <- observe "variance" (gamma c d)
   let model x = a + b * x
-  observe "ys" (isoGauss (fmap model xs) (sqrt var))
+  observe "ys" (isoGauss (fmap model xs) (sqrt v))
 
 -- | An example Bayesian sinusoidal regression model.
 sinusoidal :: [Double] -> Observable [Double]
-sinusoidal obs = do
-  a <- observe "intercept" (normal 0 10)
-  b <- observe "slope" (normal 0 10)
+sinusoidal xs = do
+  a <- observe "cosParam" (normal 0 10)
+  b <- observe "sinParam" (normal 0 10)
   v <- observe "variance" (invGamma 1 2)
   let model x =  a*cos x + b*sin x
-  observe "ys" (isoGauss (fmap model obs) (sqrt v))
+  observe "ys" (isoGauss (fmap model xs) (sqrt v))
 
 -- | The sinusoidal model prior, separated.
 prior :: Observable (Double, Double, Double)
 prior = do
-  a <- observe "intercept" (normal 0 10)
-  b <- observe "slope" (normal 0 10)
+  a <- observe "cosParam" (normal 0 10)
+  b <- observe "sinParam" (normal 0 10)
   v <- observe "variance" (invGamma 1 2)
   return (a, b, v)
 
 -- | The sinusoidal model likelihood, separated.
 likelihood :: [Double] -> (Double, Double, Double) -> Observable [Double]
-likelihood obs (a, b, v) = do
+likelihood xs (a, b, v) = do
   let model x = a*cos x + b*sin x
-  observe "ys" (isoGauss (fmap model obs) (sqrt v))
+  observe "ys" (isoGauss (fmap model xs) (sqrt v))
 
 -- | An alternative spec for the sinusoidal model.
 sinusoidalModel :: [Double] -> Observable [Double]
-sinusoidalModel obs = prior >>= likelihood obs
+sinusoidalModel xs = prior >>= likelihood xs
 
