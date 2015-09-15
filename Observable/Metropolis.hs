074ccf98d63b837eb07194cc91ee6e1b8778b313
Jared Tobin <jared@jtobin.ca>
2015-09-15 15:57:54 +1200

    Sorcery.

--- Observable/Metropolis.hs
+++ Observable/Metropolis.hs
@@ -1,8 +1,10 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Observable.Metropolis where
 
 import Control.Comonad
 import Control.Comonad.Cofree
+import Control.Monad.Primitive (PrimMonad, PrimState)
 import Control.Monad.Trans.State.Strict
 import Data.Dynamic
 import Data.Functor.Foldable
@@ -12,7 +14,7 @@ import Observable.Core
 import Observable.Distribution
 import System.Random.MWC.Probability (Prob)
 import qualified System.Random.MWC.Probability as Prob
-import System.Random.MWC (withSystemRandom, asGenIO)
+import System.Random.MWC (withSystemRandom, asGenIO, Gen, create)
 import System.IO.Unsafe (unsafePerformIO)
 
 printTop :: Show a => Cofree f a -> IO ()
@@ -34,53 +36,62 @@ data Chain a = Chain {
 type MarkovChain a = StateT (Chain a) Identity [Dynamic]
 
 -- | Initialize a Markov chain over conditional program executions.
-initializeExecution :: Typeable a => Conditioned a -> Execution a
-initializeExecution = go where
+initializeExecution
+  :: (PrimMonad m, Typeable a)
+  => Gen (PrimState m)
+  -> Conditioned a
+  -> m (Execution a)
+initializeExecution gen = go where
   go term@(Unconditioned :< f) = case f of
-    BetaF a b _ -> term =>> \w ->
-      let z = toDyn (unsafeSample (Prob.beta a b))
-      in  (extract w, z, scoreNode z f)
+    BetaF a b _ -> do
+      z <- fmap toDyn (Prob.sample (Prob.beta a b) gen)
+      return $ term =>> \w ->
+        (extract w, z, scoreNode z f)
 
-    BinomialF n p _ -> term =>> \w ->
-      let z = toDyn (unsafeSample (Prob.binomial n p))
-      in (extract w, z, scoreNode z f)
+    BinomialF n p _ -> do
+      z <- fmap toDyn (Prob.sample (Prob.binomial n p) gen)
+      return $ term =>> \w ->
+        (extract w, z, scoreNode z f)
 
-    StandardF _ -> term =>> \w ->
-      let z = toDyn (unsafeSample Prob.standard)
-      in (extract w, z, scoreNode z f)
+  go term@(Conditioned a :< f) = do
+      let z = toDyn a
+      return $ term =>> \w ->
+        (extract w, z, scoreNode z f)
 
-    NormalF m s _ -> term =>> \w ->
-      let z = toDyn (unsafeSample (Prob.normal m s))
-      in (extract w, z, scoreNode z f)
+  go w = return $ w =>> const (extract w, toDyn (), 0)
 
-  go term@(Conditioned a :< f) = term =>> \w -> let z = toDyn a in
-      (extract w, z, scoreNode z f)
+perturbExecution
+  :: PrimMonad m
+  => Gen (PrimState m)
+  -> Double
+  -> Execution a
+  -> m (Execution a)
+perturbExecution gen s = go where
+  go term@((node@Unconditioned, z, _) :< f) = do
+    p <- perturbNode gen z s
+    return $ term =>> const (node, p, scoreNode p f)
 
-  go w = w =>> const (extract w, toDyn (), 0)
+  go term@((node@Conditioned {}, z, _) :< f) =
+    return $ term =>> const (node, z, scoreNode z f)
 
-perturbExecution :: Double -> Execution a -> Execution a
-perturbExecution s = go where
-  go term@((node@Unconditioned, z, _) :< f) = term =>> \w ->
-    let p = perturbNode z s
-    in  (node, p, scoreNode p f)
+  go w@((Closed, _, _) :< _) =
+    return $ w =>> const (Closed, toDyn (), 0)
 
-  go term@((node@Conditioned {}, z, _) :< f) = term =>> const
-    (node, z, scoreNode z f)
-
-  go w@((Closed, _, _) :< _) = w =>> const (Closed, toDyn (), 0)
-
-
-perturbNode :: Dynamic -> Double -> Dynamic
-perturbNode z s
-  | dynTypeRep z == typeOf (0 :: Int) = toDyn $
-      fromJust (fromDynamic z) + unsafeSample (Prob.uniformR (-1, 1 :: Int))
-  | dynTypeRep z == typeOf (0 :: Double) = toDyn $
-      fromJust (fromDynamic z) + unsafeSample (Prob.normal 0 s)
-  | otherwise = error "perturbNode: unsupported type"
-
--- | Unsafely sample from a distribution.
-unsafeSample :: Prob IO a -> a
-unsafeSample = unsafePerformIO . withSystemRandom . asGenIO . Prob.sample
+perturbNode
+  :: PrimMonad m
+  => Gen (PrimState m)
+  -> Dynamic
+  -> Double
+  -> m Dynamic
+perturbNode gen z s
+  | dynTypeRep z == typeOf (0 :: Int) = do
+      u <- Prob.sample (Prob.uniformR (-1, 1 :: Int)) gen
+      return . toDyn $ unsafeFromDyn z + u
+  | dynTypeRep z == typeOf (0 :: Double) = do
+      u <- Prob.sample (Prob.normal 0 s) gen
+      return . toDyn $ unsafeFromDyn z + u
+  | otherwise = error $
+      "perturbNode: unsupported type, " ++ show (dynTypeRep z)
 
 -- | Calculate a probability mass/density for a given distribution and provided
 --   parameter.
@@ -88,18 +99,39 @@ scoreNode :: Dynamic -> ModelF t -> Double
 scoreNode z term = fromJust $ case term of
   BetaF a b _     -> fmap (log . densityBeta a b) (fromDynamic z)
   BinomialF n p _ -> fmap (log . densityBinomial n p) (fromDynamic z)
-  StandardF _     -> fmap (log . densityNormal 0 1) (fromDynamic z)
-  NormalF m s _   -> fmap (log . densityNormal m s) (fromDynamic z)
   ConditionF      -> Just 0
 
 -- | Score the execution of a program.
 scoreExecution :: Cofree ModelF (Node a, Dynamic, Double) -> Double
 scoreExecution = go where
   go ((_, a, s) :< f) = case f of
-    BetaF _ _ k     -> s + go (k (fromJust $ fromDynamic a))
-    BinomialF _ _ k -> s + go (k (fromJust $ fromDynamic a))
-    StandardF k     -> s + go (k (fromJust $ fromDynamic a))
-    NormalF _ _ k   -> s + go (k (fromJust $ fromDynamic a))
+    BetaF _ _ k     -> s + go (k (unsafeFromDyn a))
+    BinomialF _ _ k -> s + go (k (unsafeFromDyn a))
     ConditionF      -> s
 
+-- | Unsafely sample from a distribution.
+unsafeSample :: Prob IO a -> a
+unsafeSample = unsafePerformIO . withSystemRandom . asGenIO . Prob.sample
+
+proposalProbability s = go where
+  go :: Execution a -> Execution a -> Double
+  go ((_, z0, _) :< f) ((_, z1, _) :< _) = case f of
+    BetaF _ _ k     ->
+      let (u0, u1) = (unsafeFromDyn z0 :: Double, unsafeFromDyn z1 :: Double)
+      in  log (densityNormal u0 u1 s) + go (k u0) (k u1)
+
+    BinomialF _ _ k     ->
+      let (u0, u1) = (unsafeFromDyn z0 :: Int, unsafeFromDyn z1 :: Int)
+      in  log (1 / 3) + go (k u0) (k u1)
+
+    ConditionF -> 0
+
+--     BetaF _ _ k     -> s + go (k (unsafeFromDyn a))
+--     BinomialF _ _ k -> s + go (k (unsafeFromDyn a))
+--     StandardF k     -> s + go (k (unsafeFromDyn a))
+--     NormalF _ _ k   -> s + go (k (unsafeFromDyn a))
+--     ConditionF      -> s
+
+unsafeFromDyn :: Typeable a => Dynamic -> a
+unsafeFromDyn = fromJust . fromDynamic
 
