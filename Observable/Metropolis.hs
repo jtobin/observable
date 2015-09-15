19343427caf17018279604634c01544613eaf3e1
Jared Tobin <jared@jtobin.ca>
2015-09-15 16:57:54 +1200

    Stuff happening though not correct.

--- Observable/Metropolis.hs
+++ Observable/Metropolis.hs
@@ -1,16 +1,20 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Observable.Metropolis where
 
 import Control.Comonad
 import Control.Comonad.Cofree
+import Control.Monad (replicateM)
 import Control.Monad.Primitive (PrimMonad, PrimState)
+import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.State.Strict
 import Data.Dynamic
 import Data.Functor.Foldable
 import Data.Functor.Identity (Identity(..))
 import Data.Maybe (fromJust)
-import Observable.Core
+import Observable.Core hiding (Parameter)
 import Observable.Distribution
 import System.Random.MWC.Probability (Prob)
 import qualified System.Random.MWC.Probability as Prob
@@ -33,28 +37,56 @@ data Chain a = Chain {
   , chainExecution :: Execution a
   }
 
-type MarkovChain m a = StateT (Chain a) m [Dynamic]
+type Transition m a = StateT (Chain a) (Prob m) [Parameter]
 
--- transition = do
---   Chain s e <- get
---   proposal  <- lift perturbExecution
+metropolis :: PrimMonad m => Int -> Double -> Chain a -> Prob m [[Parameter]]
+metropolis n step = evalStateT (replicateM n (transition step))
 
+transition :: PrimMonad m => Double -> Transition m a
+transition step = do
+  currentState@(Chain currentScore current) <- get
+  proposal <- lift (perturbExecution step current)
+
+  let proposalScore     = scoreExecution proposal
+      currentToProposal = transitionProbability step current proposal
+      proposalToCurrent = transitionProbability step proposal current
+
+      ratio = exp . min 0 $
+          proposalScore + proposalToCurrent - currentScore - currentToProposal
+
+      acceptanceProbability
+        | isNaN ratio = 0
+        | otherwise   = ratio
+
+  zc <- lift Prob.uniform
+
+  if   zc < acceptanceProbability
+  then put (Chain proposalScore proposal) >> return (collectPositions proposal)
+  else return (collectPositions current)
+
+initializeChain
+  :: (Typeable a, PrimMonad m)
+  => Conditioned a
+  -> Prob m (Chain a)
+initializeChain prog = do
+  initd <- initializeExecution prog
+  let score = scoreExecution initd
+  return $ Chain score initd
 
 -- | Initialize a Markov chain over conditional program executions.
 initializeExecution
   :: (PrimMonad m, Typeable a)
-  => Gen (PrimState m)
-  -> Conditioned a
-  -> m (Execution a)
-initializeExecution gen = go where
+  => Conditioned a
+  -> Prob m (Execution a)
+initializeExecution = go where
   go term@(Unconditioned :< f) = case f of
     BetaF a b _ -> do
-      z <- fmap toDyn (Prob.sample (Prob.beta a b) gen)
+      z <- fmap toDyn (Prob.beta a b)
       return $ term =>> \w ->
         (extract w, z, scoreNode z f)
 
     BinomialF n p _ -> do
-      z <- fmap toDyn (Prob.sample (Prob.binomial n p) gen)
+      z <- fmap toDyn (Prob.binomial n p)
       return $ term =>> \w ->
         (extract w, z, scoreNode z f)
 
@@ -67,13 +99,12 @@ initializeExecution gen = go where
 
 perturbExecution
   :: PrimMonad m
-  => Gen (PrimState m)
-  -> Double
+  => Double
   -> Execution a
-  -> m (Execution a)
-perturbExecution gen s = go where
+  -> Prob m (Execution a)
+perturbExecution s = go where
   go term@((node@Unconditioned, z, _) :< f) = do
-    p <- perturbNode gen z s
+    p <- perturbNode z s
     return $ term =>> const (node, p, scoreNode p f)
 
   go term@((node@Conditioned {}, z, _) :< f) =
@@ -84,16 +115,15 @@ perturbExecution gen s = go where
 
 perturbNode
   :: PrimMonad m
-  => Gen (PrimState m)
-  -> Dynamic
+  => Dynamic
   -> Double
-  -> m Dynamic
-perturbNode gen z s
+  -> Prob m Dynamic
+perturbNode z s
   | dynTypeRep z == typeOf (0 :: Int) = do
-      u <- Prob.sample (Prob.uniformR (-1, 1 :: Int)) gen
+      u <- Prob.uniformR (-1, 1 :: Int)
       return . toDyn $ unsafeFromDyn z + u
   | dynTypeRep z == typeOf (0 :: Double) = do
-      u <- Prob.sample (Prob.normal 0 s) gen
+      u <- Prob.normal 0 s
       return . toDyn $ unsafeFromDyn z + u
   | otherwise = error $
       "perturbNode: unsupported type, " ++ show (dynTypeRep z)
@@ -114,8 +144,12 @@ scoreExecution = go where
     BinomialF _ _ k -> s + go (k (unsafeFromDyn a))
     ConditionF      -> s
 
-proposalProbability :: Double -> Execution a -> Execution a -> Double
-proposalProbability s = go where
+transitionProbability
+  :: Double       -- ^ Step size
+  -> Execution a  -- ^ Execution at current location
+  -> Execution a  -- ^ Execution at proposed location
+  -> Double       -- ^ Transition probability
+transitionProbability s = go where
   go :: Execution a -> Execution a -> Double
   go ((_, z0, _) :< f) ((_, z1, _) :< _) = case f of
     BetaF _ _ k     ->
@@ -128,6 +162,38 @@ proposalProbability s = go where
 
     ConditionF -> 0
 
+collectPositions :: Execution a -> [Parameter]
+collectPositions = go where
+  go ((Unconditioned, a, _) :< f) = case f of
+    BetaF _ _ k     -> toParameter a : go (k (unsafeFromDyn a))
+    BinomialF _ _ k -> toParameter a : go (k (unsafeFromDyn a))
+    ConditionF      -> []
+
+  go _ = []
+
+
 unsafeFromDyn :: Typeable a => Dynamic -> a
 unsafeFromDyn = fromJust . fromDynamic
 
+toParameter :: Dynamic -> Parameter
+toParameter z
+  | dynTypeRep z == typeOf (0 :: Int)    = Parameter (unsafeFromDyn z :: Int)
+  | dynTypeRep z == typeOf (0 :: Double) = Parameter (unsafeFromDyn z :: Double)
+  | otherwise = error "toParameter: unsupported type"
+
+nodesStatus = go where
+  go ((s, a, _) :< f) = case f of
+    BetaF _ _ k -> s : go (k (unsafeFromDyn a))
+    BinomialF _ _ k -> s : go (k (unsafeFromDyn a))
+    ConditionF -> []
+
+testo = go where
+  go (s :< f) = case f of
+    BetaF _ _ k -> s : go (k 0)
+    BinomialF _ _ k -> s : go (k 0)
+    ConditionF -> []
+
+data Parameter = forall a. Show a => Parameter a
+
+instance Show Parameter where
+  show (Parameter s) = show s
